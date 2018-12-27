ace92342d50e7d013201ab11e937e43283ae5edb
ceh-2000 <34041975+ceh-2000@users.noreply.github.com>
2018-12-27 16:23:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width" />
-<title>WIFI Controlled LED</title>
+<title>WIFI Controlled LEDs</title>
 </head>
        <body>
-       <center><b><font size = '20'>Control LED:</b>       
+       <center><b><font size = '20'>Control LEDs:</b>       
          <form method="get" action="gpio.php">                 
-                 <input type="submit" style = "font-size: 16 pt" value="OFF" name="off">
+                <!--                 first led -->
+ 
+                <input type="submit" style = "font-size: 16 pt" value="OFF" name="off">
                  <input type="submit" style = "font-size: 16 pt" value="ON" name="on">
                  <input type="submit" style = "font-size: 16 pt" value="BLINK" name="blink">
+<!--                 second led -->
+                <input type="submit" style = "font-size: 16 pt" value="OFF" name="off2">
+                 <input type="submit" style = "font-size: 16 pt" value="ON" name="on2">
+                 <input type="submit" style = "font-size: 16 pt" value="BLINK" name="blink2">
          </form>
    </center></font>
    </body>
